1dbb44233cdf3a94737291662cf0ca9753e169db
石殿轩 <shidianxuan@sspaas.com>
2020-12-13 17:23:23 +0800

    vue 第一节课

--- README.md
+++ README.md
@@ -1,29 +1,16 @@
-# hellow-world
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+##vuecls1:
+1. vue-cli安装（cnpm）
+2. vue 指令创建项目
+3. npm 运行vue项目的指令
+4. 工程目录中每一个文件/文件夹的作用是什么？
+5. 编辑App.vue查看页面样式
+6. 模版语法，v-bind，v-if，v-for，v-model
+7. data方法以及里面的数据
+8. methods属性以及里面的方法
+9. v-bind：class的特殊处理。
+10. 元素的样式，依然写到style标签
+11. templete标签有一个特殊限制。
+12. this
+### 作业
+1. 继续完成百度新闻
+2. 重新用vue写朋友圈/随机点名 二选一
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{message.name}}
+    <span v-bind:title="message.name">我是span标签</span>
+    <div v-for="(item,index) in arr" :key="index">{{item}}</div>
+    <div v-if="false">我是if指令</div>
+    <div v-else-if="true">我是if指令 else if 分支</div>
+    <div v-else>我是if指令的 else分支</div>
+    <!-- <div v-bind:class="[index == 1?cls1:index==2?cls2:index==3?cls3:'',cls]" v-on:click="test">点我</div> -->
+    <input type="text" v-model.number="val" />
+    <select v-model="ope">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+    </select>
+    <input type="text" v-model.number="val1" />
+    <!-- <div v-on:click="computeResult">计算</div> -->
+    <div v-if="ope == '+'">我是运算结果：{{val+val1}}</div>
+    <div v-if="ope == '-'">我是运算结果：{{val-val1}}</div>
+    <div v-if="ope == '*'">我是运算结果：{{val*val1}}</div>
+    <div v-if="ope == '/'">我是运算结果：{{val/val1}}</div>
+    <!-- <div v-else>请选择运算符</div> -->
+    <div class="item" v-for="item in news" :key="item.index">
+      <div
+        v-bind:class="[item.index==1?'cls1':item.index==2?'cls2':item.index==3?'cls3':'cls']"
+      >{{item.index}}</div>
+      <div>{{item.title}}</div>
+      <div>{{item.count}}万</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      message: {
+        name: "hello"
+      },
+      val: 0,
+      val1: 0,
+      sum: 0,
+      ope: "+",
+      isBtn: false,
+      arr: [11, 22, 33, 44, 55],
+      news: [
+        {
+          index: 1,
+          title: "31省",
+          count: 477
+        },
+        {
+          index: 2,
+          title: "嫦娥5号",
+          count: 477
+        },
+        {
+          index: 3,
+          title: "黑龙江",
+          count: 477
+        },
+        {
+          index: 4,
+          title: "夙愿案",
+          count: 477
+        }
+      ]
+    };
+  },
+  methods: {
+    test() {
+      console.log("点击了按钮");
+    },
+    computeResult() {
+      switch (this.ope) {
+        case "+":
+          this.sum = this.val + this.val1;
+          break;
+        case "-":
+          this.sum = this.val - this.val1;
+          break;
+        case "*":
+          this.sum = this.val * this.val1;
+          break;
+        case "/":
+          this.sum = this.val / this.val1;
+          break;
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -24,5 +101,29 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn {
+  color: aqua;
+  cursor: pointer;
+}
+.item {
+  width: 400px;
+  height: auto;
+  background: #f5f5f5;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+.cls {
+  color: #2c3e50;
+}
+.cls1 {
+  color: aquamarine;
+}
+.cls2 {
+  color: blue;
+}
+.cls3 {
+  color: bisque;
 }
 </style>
